88d0323dff6799f2d8e1be04bb4aeeeee182d025
Dakota Nagy <dnagy6@student.cscc.edu>
2026-07-25 05:44:32 -0400

    M1 - created AlienInvasion class, initial game loop, and set up Pygame display

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,27 @@
 import sys
 import pygame
 
+class AlienInvasion:
+    """ This is the primary class to manage game assets and behavior."""
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.running = True
+
+    def run_game(self):
+        """This will start the main loop for the game to function."""
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.flip()
+
 if __name__ == '__main__':
-    pass
+    ai = AlienInvasion()
+    ai.run_game()
