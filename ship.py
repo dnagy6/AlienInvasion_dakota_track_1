7253f277844a9330f9bb8812c72c22c32abbb1b3
Dakota Nagy <dnagy6@student.cscc.edu>
2026-07-25 07:05:52 -0400

    M1 - fixed ship rotation bug and remove unused image loading code

--- ship.py
+++ ship.py
@@ -8,16 +8,17 @@ class Ship:
     """A class to manage the player's ship."""
     
     def __init__(self, game: 'AlienInvasion') :
-        """Initialize the ship and set its starting position to 'midleft' rather than original position of 'midbottom'.
-            this will change the orientation and feel of the gameplay for the player."""
+        """Initialize the ship and set its starting position to 'midleft' rather than original position
+            of 'midbottom'. This will change the orientation and feel of the gameplay for the player."""
         self.game = game
         self.settings = game.settings
         self.screen = game.screen
         self.screen_rect = game.screen.get_rect()
 
-        self.image = pygame.image.load(self.settings.ship_file)
-        self.image = pygame.transform.scale(self.image, (
-            self.settings.ship_width, self.settings.ship_height))
+        # self.image = pygame.image.load(self.settings.ship_file)
+        # self.image = pygame.transform.scale(self.image, (
+        #     self.settings.ship_width, self.settings.ship_height))
+
         # M1 update - loading the image with transparency and cropping away all empty tranparent
         # margins around the ship artwork
         raw_image = pygame.image.load(self.settings.ship_file).convert_alpha()
@@ -29,7 +30,7 @@ class Ship:
         scaled_image = pygame.transform.scale(
             trimmed_image, (self.settings.ship_width, self.settings.ship_height)
         )
-        self.image = pygame.transform.rotate(self.image, -90)
+        self.image = pygame.transform.rotate(scaled_image, -90)
 
         # M1 update - changed the orientation of the ship to be midleft rather than midbottom
         self.rect = self.image.get_rect()
