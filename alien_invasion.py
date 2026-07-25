0392c4d3a37c9d1192fb92fe55f0031f56312868
Dakota Nagy <dnagy6@student.cscc.edu>
2026-07-25 06:10:35 -0400

    M1 - created settings file, updated background to be an image scaled to screen and requirement.txt file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,24 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """ This is the primary class to manage game assets and behavior."""
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
+
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption(self.settings.name)
+
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg, (
+            self.settings.screen_width, self.settings.screen_height))
 
         self.running = True
+        self.clock = pygame.time.Clock()
 
     def run_game(self):
         """This will start the main loop for the game to function."""
@@ -20,7 +29,10 @@ class AlienInvasion:
                     self.running = False
                     pygame.quit()
                     sys.exit()
+            self.screen.blit(self.bg, (0,0))
             pygame.display.flip()
+
+            self.clock.tick(self.settings.FPS)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
