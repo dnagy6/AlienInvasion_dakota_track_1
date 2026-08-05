8f19d6b8c378f8b75d3fb743845758c0ba49c56c
Dakota Nagy <dnagy6@student.cscc.edu>
2026-08-05 06:43:31 -0400

    M3 - game stats now updates high score and save and read from a file

--- game_stats.py
+++ game_stats.py
@@ -5,6 +5,8 @@ Purpose: Tracks and manages real-time game stats, remaining player lives, and ga
 Starter Code: Adapted from 'Python Crash Course' by Eric Matthes (3rd Edition), modified for side-scroller lifecycle.
 Date: July 29, 2026
 """
+import json
+
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -19,7 +21,30 @@ class GameStats():
         self.game = game
         self.settings = game.settings
         self.max_score = 0
+        self.init_saved_scores()
         self.reset_stats()
+
+    def init_saved_scores(self):
+        self.path = self.settings.scores_file
+        if self.path.exists() and self.path.stat.__sizeof__() > 80:
+            contents = self.path.read_text()
+            scores = json.loads(contents)
+            self.hi_score = scores.get('hi_score', 0)
+
+        else:
+            self.hi_score = 0
+            self.save_scores()
+            # save the file
+
+    def save_scores(self):
+        scores = {
+            'hi_score': self.hi_score
+        }
+        contents = json.dumps(scores, indent=4)
+        try:
+            self.path.write_text(contents)
+        except FileNotFoundError as e:
+            print(f'File Not Found: {e.value}')
 
     def reset_stats(self):
         """initialize statistics that can change during the game."""
@@ -36,11 +61,17 @@ class GameStats():
         self._update_max_score()
 
         # update high score
+        self._update_hi_score()
 
     def _update_max_score(self):
         if self.score > self.max_score:
             self.max_score = self.score
-        print(f'Max: {self.max_score}')
+        print(f'Max Score: {self.max_score}')
+
+    def _update_hi_score(self):
+        if self.score > self.hi_score:
+            self.hi_score = self.score
+        print(f'High Score: {self.hi_score}')
 
     def _update_score(self, collisions):
         for alien in collisions.values():
